d2de793110b8c3172b9ba9f1e4ddbff00ca29b58
Bhawna-1811 <91896743+Bhawna-1811@users.noreply.github.com>
2023-07-20 10:26:48 +0530

    Add files via upload

---
+++ bg.js
@@ -0,0 +1,15 @@
+function background1() {
+    document.querySelector('body').style.background = "url('./images/forest.jpg') center center / cover";
+}
+function background2() {
+    document.querySelector('body').style.background = "url('./images/library.jpg') center center / cover";
+}
+function background3() {
+    document.querySelector('body').style.background = "url('./images/sky.jpg') center center / cover";
+}
+function background4() {
+    document.querySelector('body').style.background = "url('./images/night.jpg') center center / cover";
+}
+function background5() {
+    document.querySelector('body').style.background = "url('./images/tree.jpg') center center / cover";
+}
